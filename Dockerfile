27a6c8b73c9e0d3837796736d70699e6d5a8d1e2
intern2018 <intern2018@webcorenoMacBook-Pro.local>
2018-02-15 15:16:10 +0900

    first commmit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM iwashi/docker-node8-mysql-client
+
+RUN useradd --user-group --create-home --shell /bin/false dock
+WORKDIR /app
+COPY package.json /app/package.json
+COPY package-lock.json /app/package-lock.json
+COPY server.js /app/server.js
+RUN npm install --production
+EXPOSE 8000
+USER dock
+CMD ["npm", "start"]
---
+++ server.js
@@ -0,0 +1,13 @@
+var sys = require("util");
+var http = require("http");
+var port = 8000;
+
+http
+  .createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Hello World!!");
+    sys.log("Someone access");
+    response.end();
+  })
+  .listen(port);
+sys.log("Server running at http://localhost:" + port);
